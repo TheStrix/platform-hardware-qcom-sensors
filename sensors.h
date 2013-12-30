a5887451b433cf055b0672d5aa2432f056937dc7
Jie Cheng <rockiec@codeaurora.org>
2013-12-30 11:00:17 +0800

    sensors: HAL: move the defines to headfile
    Move the sensor defines from cpp file to the headfile.
    These defines could be used by all the files in the HAL.
    
    Change-Id: I1e4de54ca1819a64f7484fe7e00e8382303733a5

--- sensors.h
+++ sensors.h
@@ -41,6 +41,20 @@ __BEGIN_DECLS
 #define SENSORS_GYROSCOPE_HANDLE		5
 #define SENSORS_PRESSURE_HANDLE			6
 
+#define MAX_SENSORS		(20)
+#define SYSFS_MAXLEN		(20)
+#define SYSFS_CLASS		"/sys/class/sensors"
+#define SYSFS_NAME		"name"
+#define SYSFS_VENDOR		"vendor"
+#define SYSFS_VERSION		"version"
+#define SYSFS_HANDLE		"handle"
+#define SYSFS_TYPE		"type"
+#define SYSFS_MAXRANGE		"max_range"
+#define SYSFS_RESOLUTION	"resolution"
+#define SYSFS_POWER		"sensor_power"
+#define SYSFS_MINDELAY		"min_delay"
+#define SYSFS_ENABLE		"enable"
+#define SYSFS_POLL_DELAY	"poll_delay"
 /*****************************************************************************/
 
 __END_DECLS
