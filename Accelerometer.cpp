cf980508ecf63d7a446f94584f7cfef343a89606
Bingzhe Cai <bingzhec@codeaurora.org>
2014-12-10 16:51:03 +0800

    sensors: set accuracy value for accelerometer and gyroscope
    Currently accuracy value of accelerometer and gyroscope are
    not set because no way to calculate this value. This change
    set default accuracy values to "High" as some application may
    need this value.
    
    CRs-fixed: 765759
    Change-Id: I7df142569fdd809eebc39c2f71c502a3f2cea075

--- Accelerometer.cpp
+++ Accelerometer.cpp
@@ -56,6 +56,7 @@ AccelSensor::AccelSensor()
 	mPendingEvent.sensor = SENSORS_ACCELERATION_HANDLE;
 	mPendingEvent.type = SENSOR_TYPE_ACCELEROMETER;
 	memset(mPendingEvent.data, 0, sizeof(mPendingEvent.data));
+	mPendingEvent.acceleration.status = SENSOR_STATUS_ACCURACY_HIGH;
 
 	if (data_fd) {
 		strlcpy(input_sysfs_path, "/sys/class/input/", sizeof(input_sysfs_path));
@@ -84,6 +85,7 @@ AccelSensor::AccelSensor(char *name)
 	mPendingEvent.sensor = SENSORS_ACCELERATION_HANDLE;
 	mPendingEvent.type = SENSOR_TYPE_ACCELEROMETER;
 	memset(mPendingEvent.data, 0, sizeof(mPendingEvent.data));
+	mPendingEvent.acceleration.status = SENSOR_STATUS_ACCURACY_HIGH;
 
 	if (data_fd) {
 		strlcpy(input_sysfs_path, SYSFS_CLASS, sizeof(input_sysfs_path));
@@ -105,6 +107,7 @@ AccelSensor::AccelSensor(SensorContext *context)
 	mPendingEvent.sensor = context->sensor->handle;
 	mPendingEvent.type = SENSOR_TYPE_ACCELEROMETER;
 	memset(mPendingEvent.data, 0, sizeof(mPendingEvent.data));
+	mPendingEvent.acceleration.status = SENSOR_STATUS_ACCURACY_HIGH;
 
 	strlcpy(input_sysfs_path, context->enable_path, sizeof(input_sysfs_path));
 	input_sysfs_path_len = strlen(input_sysfs_path);
